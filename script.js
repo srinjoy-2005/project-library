f384bcbbf7c3c6af4c10141879abdb5316b80446
Srinjoy Mukherjee <srinjoymukherjee2005@gmail.com>
2026-02-12 21:20:44 +0530

    began logic for creating new book

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function Book(author, title, pages,read){
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
     }
+    this.id = crypto.randomUUID();
     this.author=author;
     this.title=title;
     this.pages=pages;
@@ -17,6 +18,7 @@ function addBook(){
 }
 
 function displayBook() {
+  
   bookList.innerHTML = ""; // clear before re-render
 
   Library.forEach(book => {
@@ -33,8 +35,31 @@ function displayBook() {
   });
 }
 
+function createButton(){
+  const addButton=document.createElement("button");
+  addButton.setAttribute("id","add-book-button");
+  addButton.setAttribute("class","add-book-button");
 
+  buttonArea.appendChild(addButton);
+  return addButton;
+}
+
+function handleButton(){
+  if (Library.length>30){
+    alert("No more books can be added due to finiteness of space!");
+    return;
+  }
+  
+}
+
+
+//html rendering
 const bookList = document.getElementById("book-list");
+const buttonArea = document.getElementById("button-area")
 
+
+const addBookButton = createButton();
 for(let i=0;i<10;i++) addBook();
 displayBook();
+
+addBookButton.addEventListener("click", handleButton);
